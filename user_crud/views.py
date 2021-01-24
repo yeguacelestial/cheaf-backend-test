1b241c3621f720f7e622a7b9e74b3345e03c3357
Yegua Celestial <yeguacelestial@gmail.com>
2021-01-24 15:12:14 -0600

    Create Endpoint for users CRUD

--- user_crud/views.py
+++ user_crud/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from user_crud.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+        API Endpoint que permite ver o editar usuarios.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
