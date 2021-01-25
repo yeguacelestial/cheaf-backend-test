f4322b9a0fe035b75c36b88098d0b9eb5035608d
Yegua Celestial <yeguacelestial@gmail.com>
2021-01-24 21:01:20 -0600

    Handle random words endpoint

--- random_words/tests.py
+++ random_words/tests.py
@@ -22,18 +22,68 @@ class RandomWordsTest(APITestCase):
                 "plaintiff",
                 "partner",
                 "impress",
-                "partner"
+                "partner",
             ]
         }
 
         expected_response = [
-            {"prescription":"1"},
-            {"capture":"2"},
-            {"impress":"3"},
-            {"cheat":"1"},
-            {"plaintiff":"1"},
-            {"partner":"2"},
+            {"prescription":1},
+            {"capture":2},
+            {"impress":3},
+            {"cheat":1},
+            {"plaintiff":1},
+            {"partner":2},
         ]
-        response = self.client.post('/api/v1/random-words/', words)
 
-        self.assertEqual(expected_response, response.data)
+        response = self.client.post('/api/v1/random-words/', words)
+        self.assertEqual(response.data, expected_response)
+    
+
+    def test_send_less_than_ten_words(self):
+        """
+            Sends less than 10 words, and evaluates the response.
+        """
+        words = {
+            "words": [
+                "prescription",
+                "capture",
+                "impress",
+            ]
+        }
+
+        expected_response = {
+            "error": "Invalid request.",
+            "message": "Number of words required: 10."
+        }
+
+        response = self.client.post('/api/v1/random-words/', words)
+        self.assertEqual(response.data, expected_response)
+
+    
+    def test_send_more_than_ten_words(self):
+        """
+            Sends more than 10 words, and evaluates the response.
+        """
+        words = {
+            "words": [
+                "prescription",
+                "capture",
+                "impress",
+                "impress",
+                "cheat",
+                "capture",
+                "impress",
+                "plaintiff",
+                "partner",
+                "impress",
+                "partner",
+            ]
+        }
+
+        expected_response = {
+            "error": "Invalid request.",
+            "message": "Number of words required: 10."
+        }
+
+        response = self.client.post('/api/v1/random-words/', words)
+        self.assertEqual(response.data, expected_response)
--- random_words/views.py
+++ random_words/views.py
@@ -1,7 +1,11 @@
+import json
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-import json
+from rest_framework import status
+
+from .serializers import RandomWordsSerializer
 
 class RandomWords(APIView):
     """
@@ -13,5 +17,27 @@ class RandomWords(APIView):
             Sends a list of 10 random words. Returns a list with the words, and
             the number of times they appear in the list.
         """
-        # print(request.data)
-        return Response({'hello':'world'})
+        my_serializer = RandomWordsSerializer(data=request.data)
+
+        if my_serializer.is_valid():
+            # Get list of words on the request
+            word_list = my_serializer.data['words']
+            
+            response = []
+            passed_words = [] # This list saves the words already saved in response
+
+            for word in word_list:
+                if word not in passed_words:
+                    response.append({word: word_list.count(word)})
+                    passed_words.append(word)
+            
+            status_code = status.HTTP_200_OK
+        
+        else:
+            response = {
+                'error': 'Invalid request.',
+                'message': "Number of words required: 10."
+            }
+            status_code = status.HTTP_400_BAD_REQUEST
+
+        return Response(response, status_code)
